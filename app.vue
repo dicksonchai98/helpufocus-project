c429927475347ceb8211a2442c5d7f415c1983c6
chai <chaichaidickson@gmail.com>
2024-03-14 20:24:12 +0800

    1 commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <testView />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["~/assets/scss/styles.scss"],
+  //   vite: {
+  //     css: {
+  //       preprocessorOptions: {
+  //         scss: {
+  //           additionalData: '@use "~/assets/function.scss" as *;'
+  //         }
+  //       }
+  //     }
+  //   }
+  // })
+  modules: [
+    // ...
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: [
+          // 自动引入 `defineStore()`
+          "defineStore",
+          // 自动引入 `defineStore()` 并重命名为 `definePiniaStore()`
+          ["defineStore", "definePiniaStore"],
+        ],
+      },
+    ],
+  ],
+  imports: {
+    dirs: ["./stores"],
+  },
+});
